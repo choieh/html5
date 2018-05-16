55f32c149f65136732f38ad0b1c4b64eff78a9c0
choieh <bobomi66@naver.com>
2018-05-16 18:23:26 +0900

    ㅍㅍ
    ㅍㅍ

--- 포트폴리오/pf02/js/script.js
+++ 포트폴리오/pf02/js/script.js
@@ -1,10 +1,83 @@
 $(document).ready(function(){
+	
+	var check = true;
 
-	//모바일 버전 메뉴 버튼 클릭시
+	//모바일 버전 메뉴버튼 클릭시
 	$("header>.mobile>.btn").on("click",function(){
 		
-		$(".m_gnb").parent().addClass("on");
+		$(".mobile>nav").addClass("on").toggle();
+		
+		if(check == true){
+			
+			$("header>.mobile>.btn>.fa-bars").hide();
+			$("header>.mobile>.btn>.fa-times").css("display","block");	
+			
+			check = false;
+			
+		}else{
+			$("header>.mobile>.btn>.fa-bars").show();
+			$("header>.mobile>.btn>.fa-times").css("display","none");
+			
+			check = true;
+		}
 		
 	});
+	
+	
+	//맨위로 올라가는 버튼
+	$("header>.mobile>.top").on("click",function(){
+		
+		$("html,body").stop().animate({"scrollTop":0},500);
+		
+	});
+	
+	
+	//상세메뉴 버튼 클릭시 메뉴창 사라짐
+	$("header>.mobile>nav>.m_gnb>li").on("click",function(){
+		
+		$("header>.mobile>nav").removeClass("on");
+		$("header>.mobile>nav").hide();
+		
+		
+		if(check == true){
+			
+			$("header>.mobile>.btn>.fa-bars").hide();
+			$("header>.mobile>.btn>.fa-times").css("display","block");	
+			
+			check = false;
+			
+		}else{
+			$("header>.mobile>.btn>.fa-bars").show();
+			$("header>.mobile>.btn>.fa-times").css("display","none");
+			
+			check = true;
+		}
+		
+		
+	});
+	
+	
+	
 
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
